d38a7c90b6fc310005ecec2e03b98dd1429edf16
Luca Zorzi <lucazorzi91@gmail.com>
2013-01-13 21:00:16 +0100

    Corrected bug with file names containing spaces, added automatic download of SublerCLI

--- metaRenamer.sh
+++ metaRenamer.sh
@@ -35,7 +35,7 @@ function process_tvshow
 
 
 	NEW_NAME="$SHOW $SEASON""x$EPISODE $TITLE.m4v";
-	rename $SOURCE "$SOURCE_PATH/$NEW_NAME";
+	rename "$SOURCE" "$SOURCE_PATH/$NEW_NAME";
 
 	echo -e "TV show renamed to \033[33;40;5m\033[1m$NEW_NAME\033[0m";
 }
@@ -44,7 +44,7 @@ function process_tvshow
 function process_movie
 {
 	NEW_NAME="$TITLE.m4v";
-	rename $SOURCE "$SOURCE_PATH/$NEW_NAME";
+	rename "$SOURCE" "$SOURCE_PATH/$NEW_NAME";
 
 	echo -e "Movie renamed to \033[33;40;5m\033[1m$NEW_NAME\033[0m";
 }
@@ -56,12 +56,48 @@ function rename
 	return;
 }
 
+# Check if SublerCLI is available
+command -v SublerCLI >/dev/null 2>&1
+if [ $? -eq 1 ]
+then
+	echo "Error: You do not appear to have SublerCLI installed.";
+	echo "metaRenamer can try to grab the latest copy of SublerCLI for you, and install it";
+	read -p "Do you want metaRenamer do download and install SublerCLI? [y/n] " -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		echo;
+		echo "Starting download...";
+		URL=`curl -s http://lucazorzi.net/stuff/SublerCLI-URL.txt`;
+		curl -# -o /tmp/sublercli.zip $URL;
+		echo "Download completed";
+		echo -n "Extracting... "
+		unzip -qq /tmp/sublercli.zip SublerCLI -d /tmp/
+		echo "Done"
+		echo "SublerCLI has to be moved to a system directory, so you'll need to provide your password. It won't be stored."
+		sudo mv /tmp/SublerCLI /usr/bin
+		command -v SublerCLI >/dev/null 2>&1
+		if [ $? -eq 1 ]
+		then
+			echo "Something went wrong while trying to download or install SublerCLI.";
+			echo "Please go to http://code.google.com/p/subler/ and download the latest version of SublerCLI."
+			echo "Extract the zip file and move SublerCLI to /usr/bin or any other directory in your PATH."
+			exit 1;
+		else
+			echo "Installation successful"
+		fi
+		rm /tmp/SublerCLI /tmp/sublercli.zip 2> /dev/null
+	else
+		exit 1;
+	fi
+fi
+
 
 # Check if user has supplied an input file
 if [ $# -lt 1 ]
 then
 	usage;
 fi
+
 
 # Check if file exists
 if [ ! -f "$1" ]
